17502288a072cd59178fa8d81b13e2cec62be24d
salazarbrandon1257 <salazarbrandon1257@gmail.com>
2020-09-19 17:59:47 -0500

    testing change

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -282,7 +282,7 @@ public class MyLinkedList<AnyType> implements Iterable<AnyType>
         public Node<AnyType>   next;
     }
     
-    private int theSize;
+    private int theSize; 
     private Node<AnyType> beginMarker;
     private Node<AnyType> endMarker;
 }
