033eb225e2ad7cf61e4f87e3f5cb4ff10cf88377
salazarbrandon1257 <salazarbrandon1257@gmail.com>
2020-09-20 16:51:12 -0500

    added peek, push, and pop

--- MyStack.java
+++ MyStack.java
@@ -1,10 +1,31 @@
 import java.util.ArrayList;
 
-public class MyStack {
-    ArrayList<Integer> arr;
+public class MyStack<AnyType>{
+    ArrayList<AnyType> arr;
     MyStack(){
-        arr = new ArrayList<Integer>();
+        arr = new ArrayList<>();
     }
-    
+    public void push(AnyType x){
+        arr.add(x);
+    }
+    public AnyType peek(){
+        System.out.println(arr.get(arr.size() - 1));
+        return arr.get(arr.size() - 1);
+    }
+    public AnyType pop(){
+        return  arr.remove(arr.size() - 1);
+    }
+
 
 }
+class TestMystack
+{
+    public static void main( String [ ] args )
+    {
+        MyStack<Integer> stack = new MyStack<Integer>( );
+        stack.push(1);
+        stack.push(2);
+        stack.peek();
+    }
+}
+
